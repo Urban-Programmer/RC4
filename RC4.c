7646f0aa988c8aae437d6ea6e675bbbc7cbf87bb
Chris Wilson <awil17@outlook.com>
2023-04-29 17:10:24 -0400

    Including RC4 // Crypto lib

--- RC4.c
+++ RC4.c
@@ -3,6 +3,8 @@
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
+#include <openssl/rc4.h>
+
 
 /*
     Program should take the following inputs from user:
@@ -12,21 +14,41 @@
     -- output file
 */
 
+
+
 // Will probably move these functions to their seperate files
 void salt(){
     //Salt must occur before encryption
 
 }
 
-void decrypt(){
-    //Decrypt AES
 
+// char *input_path, char *output_path, char* salt, 
+void RC4(){ 
+    unsigned char key[] = "Hello";
+    unsigned char plaintext[] = "Hello World!";
+    unsigned char cipher[sizeof(plaintext)];
+
+    int key_len = strlen(key);
+    int plaintext_len = strlen(plaintext);
+
+    //RC4 Key creation
+    RC4_KEY rc4_key;
+    RC4_set_key(&rc4_key, key_len, key);
+
+    // Cipher Text
+    RC4(&rc4_key, plaintext_len, plaintext, cipher);
+
+    printf("Encrypted data: ");
+    for (int i = 0; i < plaintext_len; i++) {
+        printf("%02x", cipher[i]);
+    }
+    printf("\n");
+    printf("Decrypted data: %s\n", plaintext);
+
+ 
 }
 
-void encrypt(){
-    //Implement AES
-
-}
 
 int main(int argc, char *argv[]) {
 
@@ -43,8 +65,6 @@ int main(int argc, char *argv[]) {
     
 
     //Above Variables will require error checking @ some point in time
-
-
     FILE *output_file = fopen(output_path,"w");
 
     //instead this should encrypt the file
@@ -54,6 +74,8 @@ int main(int argc, char *argv[]) {
     //
     fwrite(test, 1, strlen(test), output_file);
     fclose(output_file);
+
+    RC4();
 
 
 
